e8c9c0db62b4d429773c1692f3ea558c7d96e8b5
AmirAiman-123 <memermemer24@gmail.com>
2025-08-10 01:48:43 +0800

    again

--- src/api.js
+++ src/api.js
@@ -1,16 +1,31 @@
 // File: frontend/src/api.js
 
-import axios from 'axios';
+//import axios from 'axios';
 
 // Create an instance of axios with a custom configuration
-const api = axios.create({
-  withCredentials: true,
-});
+//const api = axios.create({
+//  withCredentials: true,
+//});
 
 
 //const api = axios.create({
 //  baseURL: 'http://127.0.0.1:5000', // Your public ngrok URL
 //  withCredentials: true,
 //});
+
+//export default api;
+
+// File: AuraDrive/frontend/src/api.js (Production Ready)
+
+import axios from 'axios';
+
+// During local development, this will be falsy, and it will use the proxy.
+// When deployed on Vercel, it will use the value of the environment variable.
+const baseURL = process.env.REACT_APP_API_BASE_URL;
+
+const api = axios.create({
+  baseURL: baseURL,
+  withCredentials: true,
+});
 
 export default api;
